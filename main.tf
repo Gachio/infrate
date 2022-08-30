7c52ccfd1dd26bf4081b75821e7fe10bcf6b476a
Arm Bun <bellotaophyc@outlook.com>
2022-08-30 20:09:00 +0000

    update the resource name

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "1infra" {
+resource "aws_instance" "oneinfra" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 }
