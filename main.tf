fb11df3b5967379b1591eb26047c6f98313c0a4e
Arm Bun <bellotaophyc@outlook.com>
2022-08-30 20:07:22 +0000

    resource config

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "1infra" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
